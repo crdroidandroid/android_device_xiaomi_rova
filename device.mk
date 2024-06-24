c478f9b5c69b047d641c31ba3e7e050382128a2d
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-24 16:47:29 +0800

    rova: Migrate to QTI USB Gadget 1.2 HIDL
    Change-Id: I6adbca2fab9a3fe0477d17f9786166828b786710

--- device.mk
+++ device.mk
@@ -486,9 +486,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.xiaomi_rova
 
-# USB HAL
+# USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.basic
+    usb_compositions.conf \
+    android.hardware.usb@1.3-service.basic \
+    android.hardware.usb.gadget@1.2-service-qti
 
 # Userspace Reboot
 $(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
